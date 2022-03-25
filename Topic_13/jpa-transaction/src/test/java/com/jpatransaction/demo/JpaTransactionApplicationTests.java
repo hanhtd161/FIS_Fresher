7774cc58f0bb8e44eee76a81005c3fc9b78ff6e2
hanhtd161 <hanhtd161@gmail.com>
2022-03-25 08:41:30 +0700

    add practice jpa v1

---
+++ Topic_13/jpa-transaction/src/test/java/com/jpatransaction/demo/JpaTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpatransaction.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Topic_13/springjpa/src/main/java/com/springjpa/demo/repository/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.springjpa.demo.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.springjpa.demo.entity.Employee;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+    Employee findByEmpNo(String empNo);
+
+    List<Employee> findByFullNameLike(String fullName);
+
+    List<Employee> findByHireDateGreaterThan(Date hireDate);
+
+    @Query("SELECT coalesce(max(e.id), 0) FROM Employee e")
+    Long getMaxId();
+}
